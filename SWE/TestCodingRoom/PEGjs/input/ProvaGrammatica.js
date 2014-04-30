ba54877de53cffa8de921095503c19cd2a86262e
masokin <maso.michele@gmail.com>
2014-04-30 15:02:30 +0200

    Aggiornamento grammatica

--- SWE/TestCodingRoom/PEGjs/input/ProvaGrammatica.js
+++ SWE/TestCodingRoom/PEGjs/input/ProvaGrammatica.js
@@ -1,26 +1,48 @@
+
 start
   = dsl
 dsl
   =ind c:(indexdo)* doc d:(showdo)* end
 ind
-  = string:"index do\n" {return "column";}
+  = string:"index do" LineTerminatorSequence+ {return "column";}
 doc
-  = string:"show do\n" {return "row";}
+  = string:"show do"LineTerminatorSequence+ {return "row";}
 indexdo
-  =(" ")*column(" ")*ris:token'\n'
+  =column e:etichetta ris:token LineTerminatorSequence+
   
 column
-  =string: "column:" 
+  =WhiteSpace* string: "column:" WhiteSpace*
 
+ etichetta
+  = digits:[a-z]* {
+return digits.join("");
+}
+  
 token
-  =digits:[a-z]+ {
+  =digits:[a-z]* {
 return digits.join("");
 }
 
 showdo
-  =(" ")*row(" ")*ris:token'\n' {return ris;}
+  =WhiteSpace *row WhiteSpace *ris:token LineTerminatorSequence+ {return ris;}
 
 row
   =stringa: "row:"
 end
 =string: "end"
+
+WhiteSpace "whitespace"
+  = "\t"
+  / "\v"
+  / "\f"
+  / " "
+  / "\u00A0"
+  / "\uFEFF"
+  / "Zs"
+
+LineTerminatorSequence "end of line"
+  = "\n" 
+  / "\r\n"
+  / "\r"
+  / "\u2028"
+  / "\u2029"
