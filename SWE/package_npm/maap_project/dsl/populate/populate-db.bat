7ff0d9d8a7325f85e94dfe3202c378f7d1a1e4d6
Fabio Miotto <fabietto.mi8@gmail.com>
2014-06-05 13:31:56 +0200

    aggiornato db e dsl populate

--- SWE/package_npm/maap_project/dsl/populate/populate-db.bat
+++ SWE/package_npm/maap_project/dsl/populate/populate-db.bat
@@ -1,4 +1,5 @@
 mongoimport --host localhost --port 27017 --db dati --upsert --collection teams < teams.json
+mongoimport --host localhost --port 27017 --db dati --upsert --collection coaches < coaches.json
 mongoimport --host localhost --port 27017 --db dati --upsert --collection members < members.json
 mongoimport --host localhost --port 27017 --db dati --upsert --collection supermarket < supermarket.json
 mongoimport --host localhost --port 27017 --db utenti --upsert --collection users < users.json
