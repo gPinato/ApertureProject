734662c4665b4b478887fd341c308f793c318960
the-AjK <ajk@altervista.org>
2014-06-06 16:49:39 +0200

    populate multipli

--- SWE/package_npm/maap_project/dsl/populate/populate-db.bat
+++ SWE/package_npm/maap_project/dsl/populate/populate-db.bat
@@ -1,6 +1,6 @@
-mongoimport --host localhost --port 27017 --db dati --upsert --collection teams < teams.json
-mongoimport --host localhost --port 27017 --db dati --upsert --collection coaches < coaches.json
-mongoimport --host localhost --port 27017 --db dati --upsert --collection members < members.json
-mongoimport --host localhost --port 27017 --db dati --upsert --collection supermarket < supermarket.json
-mongoimport --host localhost --port 27017 --db dati --upsert --collection positions < positions.json
-mongoimport --host localhost --port 27017 --db utenti --upsert --collection users < users.json
+mongoimport --host localhost --port 27017 --db dati --drop --collection teams < teams.json
+mongoimport --host localhost --port 27017 --db dati --drop --collection coaches < coaches.json
+mongoimport --host localhost --port 27017 --db dati --drop --collection members < members.json
+mongoimport --host localhost --port 27017 --db dati --drop --collection supermarket < supermarket.json
+mongoimport --host localhost --port 27017 --db dati --drop --collection positions < positions.json
+mongoimport --host localhost --port 27017 --db utenti --drop --collection users < users.json
