585283b9ccb4c19a0c138a993485850c578ee84f
Fabio Miotto <fabietto.mi8@gmail.com>
2014-06-12 12:05:32 +0200

    aggiunto addQuery per indici

--- SWE/package_npm/maap_server/modelServer/dataManager/IndexManager/IndexManager.js
+++ SWE/package_npm/maap_server/modelServer/dataManager/IndexManager/IndexManager.js
@@ -28,9 +28,49 @@ var getModel = function(collection_name) {
 	return -1;
 }
 
-exports.addQuery = function(collection_name, select, callback) {
+exports.addQuery = function(collection_name, select) {
 	var queryModel = require('../../Database/MongooseDBFramework').query;
+	queryModel.find(collection_name, function(err,data){
+		if(err)
+			console.log('Impossibile recuperare lista query con la collection: '+collection_name);
+		else
+			if(data){
+				for(var i = 0;i<data.length;i++){
+					if(data[i].select.length == select.length){
+						var countmatch = 0;
+						for(var j = 0;j<select.length;j++){
+							if(data[i].select[select[j]] != undefined)
+								countmatch++;		
+						}
+						if(contamatch == select.length){
+							var counter = data[i].counter++;
+							queryModel.update(select, {$set:{counter: counter}}).exec(function(err,count){
+								if(err){console.log('update counter fallito'); }
+								if(count==0){
+									console.log('impossibile aggiornare la query.'); 
+								}
+							});
+						}
+						else{
+							var criteria = new queryModel({collection_name:collection_name, select:select, counter: 0});
+							var query = criteria.save(function(err){
+								if(err)
+									console.log('inserimento query fallito');
+							});
+						}
+						
+					}
+				}//for
+			}//if
+			else{
+				var criteria = new queryModel({collection_name:collection_name, select:select, counter: 0});
+					var query = criteria.save(function(err){
+						if(err)
+							console.log('inserimento query fallito');
+					});
+			}
 	
+	});
 
 }
 
