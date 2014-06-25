4f6d1ea68720f8ac9ed27e81affde3826cfc68e2
the-AjK <ajk@altervista.org>
2014-06-25 11:37:37 +0200

    aggiunto populate mongolab

---
+++ SWE/package_npm/maap_project/dsl/populate/populate-db-mongolab.bat
@@ -0,0 +1,6 @@
+mongoimport --host ds045897.mongolab.com --port 45897 -u admin -p apertureadmin --db dbanalysis --drop --collection teams --file teams.json
+mongoimport --host ds045897.mongolab.com --port 45897 -u admin -p apertureadmin --db dbanalysis --drop --collection coaches --file coaches.json
+mongoimport --host ds045897.mongolab.com --port 45897 -u admin -p apertureadmin --db dbanalysis --drop --collection members --file members.json
+mongoimport --host ds045897.mongolab.com --port 45897 -u admin -p apertureadmin --db dbanalysis --drop --collection supermarket --file supermarket.json
+mongoimport --host ds045897.mongolab.com --port 45897 -u admin -p apertureadmin --db dbanalysis --drop --collection positions --file positions.json
+mongoimport --host ds039017.mongolab.com: --port 39017 -u user -p apertureuser --db dbframework --drop --collection users --file users.json
