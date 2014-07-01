4478d0a35ca233f9f8a78e981ce094d652e1e502
the-AjK <ajk@altervista.org>
2014-07-01 10:59:22 +0200

    aggiornato installer

--- SWE/package_npm/maap_installer/maapertureInstaller.js
+++ SWE/package_npm/maap_installer/maapertureInstaller.js
@@ -1,3 +1,20 @@
+#!/usr/bin/env node
+
+/**
+ * File: maapertureInstaller.js
+ * Module: maap_installer
+ * Author: Alberto Garbui
+ * Created: 02/05/14
+ * Version: 0.1
+ * Description: maaperture installer
+ * Modification History:
+ ==============================================
+ * Version | Changes
+ ==============================================
+ * 0.1 File creation
+ ==============================================
+ */
+ 
 "use strict";
 
 var fs = require('fs');
