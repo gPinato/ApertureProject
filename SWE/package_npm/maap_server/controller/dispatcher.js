e70994a1ea97e19215894c472b27c13a13f901cc
the-AjK <ajk@altervista.org>
2014-07-01 13:32:52 +0200

    aggiunto procfile per heroku

--- SWE/package_npm/maap_server/controller/dispatcher.js
+++ SWE/package_npm/maap_server/controller/dispatcher.js
@@ -30,7 +30,6 @@ var dispatcherInit = function (app) {
 	
 	//gestione collections e documents
 	dispatcher.get('/api/collection/list', passport.checkAuthenticated, datamanager.sendCollectionsList);
-	//dispatcher.get('/api/collection/list', passport.checkAuthenticated, indexmanager.createIndex);
 	dispatcher.get('/api/collection/:col_id', passport.checkAuthenticated, datamanager.sendCollection);
 	dispatcher.get('/api/collection/:col_id/:doc_id', passport.checkAuthenticated, datamanager.sendDocument);
 	//dispatcher.get('/api/collection/:col_id/:doc_id/edit', passport.checkAuthenticatedAdmin, datamanager.sendDocumentEdit);
