cc31432bd350207202b8e13f4a2fdb61d1837639
the-AjK <ajk@altervista.org>
2014-07-03 11:11:46 +0200

    aggiornati json per populate il db +  spostato cartella populate fuori dal progetto

--- SWE/package_npm/maap_project/dsl/populate/populate-db.bat
+++
@@ -1,6 +0,0 @@
-mongoimport --host localhost --port 27017 --db dati --drop --collection teams < teams.json
-mongoimport --host localhost --port 27017 --db dati --drop --collection coaches < coaches.json
-mongoimport --host localhost --port 27017 --db dati --drop --collection members < members.json
-mongoimport --host localhost --port 27017 --db dati --drop --collection supermarket < supermarket.json
-mongoimport --host localhost --port 27017 --db dati --drop --collection positions < positions.json
-mongoimport --host localhost --port 27017 --db utenti --drop --collection users < users.json
