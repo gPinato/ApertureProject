be2373df519a3ded29888899f6ff69cb494afe1f
Jack <giacomo.pinato@gmail.com>
2014-07-03 22:34:20 +0200

    Test n fix

--- SWE/package_npm/maap_project/app/scripts/controllers/CollectionCtrl.js
+++ SWE/package_npm/maap_project/app/scripts/controllers/CollectionCtrl.js
@@ -77,8 +77,12 @@ angular.module('maaperture').controller('CollectionCtrl', function ($scope, $rou
     init();
 
     //funzione per stampare correttamente il numero di pagine
-    $scope.numerify = function (num) {
-        return new Array(num);
+    $scope.numerify = function () {
+        //se ho meno di 10 pagine posso farle vedere tutte
+        if( $scope.pages < 10){
+        return new Array($scope.pages);
+        }
+
     };
     //Torna alla pagina precedente
     $scope.previousPage = function () {
@@ -136,6 +140,81 @@ angular.module('maaperture').controller('CollectionCtrl', function ($scope, $rou
                 alert("Qualcosa è andato storto..");
             }
         );
+    };
+
+    //=====================================================================
+    $scope.range = function() {
+
+        var rangeSize = 4;
+
+        var ps = [];
+
+        var start;
+
+        start = $scope.currentPage;
+
+        if ( start > $scope.pageCount()-rangeSize ) {
+
+            start = $scope.pageCount()-rangeSize+1;
+
+        }
+
+        for (var i=start; i<start+rangeSize; i++) {
+
+            ps.push(i);
+
+        }
+
+        return ps;
+
+    };
+
+
+    $scope.prevPage = function() {
+
+        if ($scope.currentPage > 0) {
+
+            $scope.currentPage--;
+
+        }
+    };
+
+
+    $scope.DisablePrevPage = function() {
+
+        return $scope.currentPage === 0 ? "disabled" : "";
+
+    };
+
+
+    $scope.pageCount = function() {
+
+        return Math.ceil($scope.datalists.length/$scope.itemsPerPage)-1;
+
+    };
+
+
+    $scope.nextPage = function() {
+
+        if ($scope.currentPage > $scope.pageCount()) {
+
+            $scope.currentPage++;
+
+        }
+    };
+
+
+    $scope.DisableNextPage = function() {
+
+        return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+
+    };
+
+
+    $scope.setPage = function(n) {
+
+        $scope.currentPage = n;
+
     };
 
 });
--- SWE/package_npm/maap_project/test/spec/controllers/CollectionCtrl_test.js
+++ SWE/package_npm/maap_project/test/spec/controllers/CollectionCtrl_test.js
@@ -62,7 +62,57 @@ describe('Controller: CollectionCtrl', function () {
         });
     }));
 
+    it('should initialize data correctly', function () {
+        expect(scope.current_sorted_column).to.equal(null);
+        expect(scope.current_sort).to.equal(null);
+        expect(scope.column_original_name.length).to.equal(0);
+        expect(scope.current_page).to.equal(0);
+        expect(scope.current_collection).to.equal(routeParams.col_id);
+        expect(scope.rows.length).to.equal(0);
+    });
 
+
+    it('should numerify correctly', function () {
+        var array = scope.numerify(3);
+        expect(array.length).to.equal(3);
+        array = scope.numerify(0);
+        expect(array.length).to.equal(0);
+    });
+    it('should go to the correct page', function () {
+        scope.current_page=2;
+        scope.toPage(4);
+        expect(scope.current_page).to.equal(4);
+
+    });
+    it('should increase page correctly', function () {
+        scope.pages = 3;
+        scope.current_page=0;
+        scope.nextPage();
+        expect(scope.current_page).to.equal(1);
+        scope.current_page= scope.pages - 1 ;
+        scope.nextPage();
+        expect(scope.current_page).to.equal(scope.pages - 1);
+    });
+    it('should decrease page correctly', function () {
+        scope.pages = 3;
+        scope.current_page=0;
+        scope.previousPage();
+        expect(scope.current_page).to.equal(0);
+        scope.current_page= scope.pages - 1 ;
+        scope.previousPage();
+        expect(scope.current_page).to.equal(scope.pages - 2);
+    });
+    it('should change sort correctly', function () {
+        scope.current_sort = "asc"
+        scope.current_sorted_column = 0;
+
+        scope.columnSort(1);
+        expect(scope.current_sort).to.equal("asc");
+        expect(scope.current_sorted_column).to.equal(1);
+        scope.columnSort(1);
+        expect(scope.current_sort).to.equal("desc");
+
+    });
 
     it('should load data from the services', function () {
         expect(scope.data.length).to.not.equal(0);
