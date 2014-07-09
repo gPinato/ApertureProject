97f9f40bf7d615167deb8e642e04bb8711aa506b
the-AjK <ajk@altervista.org>
2014-07-09 11:51:33 +0200

    aggiornati test DSLmanager dopo la modifica al DSLManager.js

--- SWE/package_npm/test/ModelServer/DSL/DSLManager.test.js
+++ SWE/package_npm/test/ModelServer/DSL/DSLManager.test.js
@@ -25,15 +25,15 @@ var DSLManager = require("../../../maap_server/modelServer/DSL/DSLManager.js");
 describe("Test generateFunction: ", function() {
 		
 	it("deve generare una funzione corretta per traformazioni di campi dati con nomi singoli", function() {
-		var transformation = {name: 'age', transformation: 'age = age*2'};
-		var expectedResult = '//maaperture auto-generated function for item \'age\':\n\nexports.transformation = function(age) {\nage = age*2\nreturn age;\n}'
+		var transformation = {name: 'age', transformation: 'age = age * 2;'};
+		var expectedResult = '//maaperture auto-generated function for item \'age\':\n\nexports.transformation = function(age) {\nvar originalValue = age;\ntry{\nage = age * 2;\n}catch(err){\nconsole.log(\'transformation error: \' + err);\nage = \'transformation failed, check your DSL [ \' + originalValue + \' ]\';\n}\nreturn age;\n}\n';
 		var result = DSLManager.generateFunction(transformation);
 		expect(result).to.equal(expectedResult);
     }),
 	
 	it("deve generare una funzione corretta per traformazioni di campi dati con nomi composti", function() {
-		var transformation = {name: 'coach.age', transformation: 'age = age*2'};
-		var expectedResult = '//maaperture auto-generated function for item \'age\':\n\nexports.transformation = function(age) {\nage = age*2\nreturn age;\n}'
+		var transformation = {name: 'coach.age', transformation: 'age = age * 2;'};
+		var expectedResult = '//maaperture auto-generated function for item \'age\':\n\nexports.transformation = function(age) {\nvar originalValue = age;\ntry{\nage = age * 2;\n}catch(err){\nconsole.log(\'transformation error: \' + err);\nage = \'transformation failed, check your DSL [ \' + originalValue + \' ]\';\n}\nreturn age;\n}\n';
 		var result = DSLManager.generateFunction(transformation);
 		expect(result).to.equal(expectedResult);
     });
