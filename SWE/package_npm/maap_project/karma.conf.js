78d1437a871798b54b71ec48d2ea83c4f39d8dc5
Mattia Sorgato <mattia.sorgato@gmail.com>
2014-07-10 11:32:52 +0200

    bè, è un passo avanti. bisogna dare un bower install prima del test. ora jasmine gira.

--- SWE/package_npm/maap_project/karma.conf.js
+++ SWE/package_npm/maap_project/karma.conf.js
@@ -24,10 +24,10 @@ module.exports = function(config) {
           'app/bower_components/angular-resource/angular-resource.js',
           'app/bower_components/angular-cookies/angular-cookies.js',
           'app/bower_components/angular-sanitize/angular-sanitize.js',
-          'app/bower_components/angular-route/angular-route.js'
+          'app/bower_components/angular-route/angular-route.js',
           'app/scripts/*.js',
-          'app/scripts/**/*.js',
-          'test/mock/**/*.js',
+          'app/scripts/controllers/*.js',
+          //'test/mock/**/*.js',
           'test/spec/controllers/*.js',
       ],
 
